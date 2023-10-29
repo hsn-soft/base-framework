254cf72dc749fb5b3277dd71a409c71e0172ff8f
Hasan H. SAHIN <hasansahin@araskargo.com.tr>
2023-10-30 00:16:31 +0300

    v2024.6.2

--- src/HsnSoft.Base.MultiTenancy/HsnSoft/Base/MultiTenancy/BaseMultiTenancyServiceCollectionExtensions.cs
+++ src/HsnSoft.Base.MultiTenancy/HsnSoft/Base/MultiTenancy/BaseMultiTenancyServiceCollectionExtensions.cs
@@ -7,7 +7,8 @@ public static class BaseMultiTenancyServiceCollectionExtensions
     public static IServiceCollection AddBaseMultiTenancyServiceCollection(this IServiceCollection services)
     {
         services.AddTransient<ICurrentTenant, CurrentTenant>();
-        services.AddTransient<ICurrentTenantAccessor>(sp => AsyncLocalCurrentTenantAccessor.Instance);
+        // services.AddTransient<ICurrentTenantAccessor>(sp => AsyncLocalCurrentTenantAccessor.Instance);
+        services.AddTransient<ICurrentTenantAccessor>(sp=> new BasicCurrentTenantAccessor(sp));
 
         return services;
     }
