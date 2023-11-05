edb08411b27ed5db7499818ac599dd9bf8aa8372
Hasan H. SAHIN <hasansahin@araskargo.com.tr>
2023-11-05 13:49:21 +0300

    v2024.6.4

--- src/HsnSoft.Base.Authorization/HsnSoft/Base/Authorization/Permissions/RolePermissionValueProvider.cs
+++ src/HsnSoft.Base.Authorization/HsnSoft/Base/Authorization/Permissions/RolePermissionValueProvider.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using System.Threading.Tasks;
+using HsnSoft.Base.MultiTenancy;
 using HsnSoft.Base.Security.Claims;
 
 namespace HsnSoft.Base.Authorization.Permissions;
@@ -9,9 +10,11 @@ public class RolePermissionValueProvider : PermissionValueProvider
     public const string ProviderName = "R";
 
     public override string Name => ProviderName;
+    protected ICurrentTenant CurrentTenant { get; }
 
-    public RolePermissionValueProvider(IPermissionStore permissionStore) : base(permissionStore)
+    public RolePermissionValueProvider(IPermissionStore permissionStore, ICurrentTenant currentTenant) : base(permissionStore)
     {
+        CurrentTenant = currentTenant;
     }
 
     public override async Task<PermissionGrantResult> CheckAsync(PermissionValueCheckContext context)
@@ -25,7 +28,21 @@ public class RolePermissionValueProvider : PermissionValueProvider
 
         if (roles == null || !roles.Any())
         {
-            return PermissionGrantResult.Undefined;
+            //return PermissionGrantResult.Undefined;
+
+            var clientId = context.Principal?.FindFirst(BaseClaimTypes.ClientId)?.Value ?? context.Principal?.FindFirst("client_id")?.Value;
+
+            if (clientId == null)
+            {
+                return PermissionGrantResult.Undefined;
+            }
+
+            using (CurrentTenant.Change(null))
+            {
+                return await PermissionStore.IsGrantedAsync(context.Permission.Name, Name, clientId)
+                    ? PermissionGrantResult.Granted
+                    : PermissionGrantResult.Undefined;
+            }
         }
 
         foreach (var role in roles.Distinct())
