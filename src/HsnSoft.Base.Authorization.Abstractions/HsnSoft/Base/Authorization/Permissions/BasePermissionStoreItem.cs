f079687ead806db7623b256a8c2956dcb948a0f4
Hasan H. SAHIN <hasansahin@araskargo.com.tr>
2023-11-08 00:38:18 +0300

    v2024.6.9

--- src/HsnSoft.Base.Authorization.Abstractions/HsnSoft/Base/Authorization/Permissions/BasePermissionStoreItem.cs
+++ src/HsnSoft.Base.Authorization.Abstractions/HsnSoft/Base/Authorization/Permissions/BasePermissionStoreItem.cs
@@ -1,11 +1,7 @@
-using System;
-
 namespace HsnSoft.Base.Authorization.Permissions;
 
 public class BasePermissionStoreItem
 {
-    public Guid? TenantId { get; set; }
-
     public string Name { get; set; }
 
     public string ProviderName { get; set; }
