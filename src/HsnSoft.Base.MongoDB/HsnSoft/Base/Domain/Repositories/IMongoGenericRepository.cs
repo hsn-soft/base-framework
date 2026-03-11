dd88fc45ccbd374d42355e6119b30ffd07f6581b
Hasan SAHIN <hasansahin@araskargo.com.tr>
2026-03-12 00:29:13 +0300

    ok

--- src/HsnSoft.Base.MongoDB/HsnSoft/Base/Domain/Repositories/IMongoGenericRepository.cs
+++ src/HsnSoft.Base.MongoDB/HsnSoft/Base/Domain/Repositories/IMongoGenericRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading;
@@ -15,7 +16,24 @@ public interface IMongoGenericRepository<TEntity, in TKey> : IGenericRepository<
     ITrackingMongoCollection<TEntity> GetCollection();
     IQueryable<TEntity> GetQueryable();
 
+    IClientSessionHandle StartSession(ClientSessionOptions options = null, CancellationToken cancellationToken = default);
+    Task<IClientSessionHandle> StartSessionAsync(ClientSessionOptions options = null, CancellationToken cancellationToken = default);
+
+    Task<TEntity> GetByIdAsync(IClientSessionHandle session, TKey id, CancellationToken cancellationToken = default);
+
+    Task InsertAsync(IClientSessionHandle session, TEntity entity, CancellationToken cancellationToken = default);
+    Task InsertManyAsync(IClientSessionHandle session, IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+
+    Task UpdateAsync(IClientSessionHandle session,TEntity entity, CancellationToken cancellationToken = default);
+    Task UpdateManyAsync(IClientSessionHandle session, IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+
     Task<long> UpdateByExpressionAsync(
+        Expression<Func<TEntity, bool>> predicate,
+        Func<UpdateDefinitionBuilder<TEntity>, UpdateDefinition<TEntity>> set,
+        CancellationToken cancellationToken = default);
+
+    Task<long> UpdateByExpressionAsync(
+        IClientSessionHandle session,
         Expression<Func<TEntity, bool>> predicate,
         Func<UpdateDefinitionBuilder<TEntity>, UpdateDefinition<TEntity>> set,
         CancellationToken cancellationToken = default);
