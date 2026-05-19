e0c0a575f4c2f873df8fa0cc5ae00eb2b581236c
Hasan SAHIN <hasansahin@araskargo.com.tr>
2026-05-19 15:44:13 +0300

    security log clear

--- src/HsnSoft.Base.Security/HsnSoft/Base/Security/BaseSecurityServiceCollectionExtensions.cs
+++ src/HsnSoft.Base.Security/HsnSoft/Base/Security/BaseSecurityServiceCollectionExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using HsnSoft.Base.Security.Encryption;
-using HsnSoft.Base.SecurityLog;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HsnSoft.Base.Security;
@@ -9,15 +8,6 @@ public static class BaseSecurityServiceCollectionExtensions
     public static IServiceCollection AddBaseSecurityEncryptionCollection(this IServiceCollection services)
     {
         services.AddSingleton<IStringEncryptionService, StringEncryptionService>();
-
-        return services;
-    }
-
-    public static IServiceCollection AddBaseSecurityLogCollection(this IServiceCollection services)
-    {
-        services.AddTransient<ISecurityLogStore, SimpleSecurityLogStore>();
-        services.AddSingleton<ISecurityLogManager, DefaultSecurityLogManager>();
-
 
         return services;
     }
--- src/HsnSoft.Base.Security/HsnSoft/Base/SecurityLog/ISecurityLogManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Threading.Tasks;
-
-namespace HsnSoft.Base.SecurityLog;
-
-public interface ISecurityLogManager
-{
-    Task SaveAsync(Action<SecurityLogInfo> saveAction = null);
-}
