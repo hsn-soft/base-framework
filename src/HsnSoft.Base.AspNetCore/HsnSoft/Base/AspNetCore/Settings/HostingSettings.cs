8b717fd6d8590424fc38150b8fd953e3205ac0dd
Hasan SAHIN <hasansahin@araskargo.com.tr>
2026-06-01 19:19:35 +0300

    default logger false

--- src/HsnSoft.Base.AspNetCore/HsnSoft/Base/AspNetCore/Settings/HostingSettings.cs
+++ src/HsnSoft.Base.AspNetCore/HsnSoft/Base/AspNetCore/Settings/HostingSettings.cs
@@ -2,9 +2,8 @@ namespace HsnSoft.Base.AspNetCore.Settings;
 
 public class HostingSettings
 {
-    public bool IsEnabledRequestResponseLogger { get; set; } = true;
+    public bool IsEnabledRequestResponseLogger { get; set; } = false;
     public bool IsEnabledHealthCheckRequestLogger { get; set; } = false;
-
     public int MaxLoggedRequestBodySizeBytes { get; set; } = 8 * 1024;
     public int MaxLoggedResponseBodySizeBytes { get; set; } = 8 * 1024;
     public int MaxLoggedHeaderValueLength { get; set; } = 128;
