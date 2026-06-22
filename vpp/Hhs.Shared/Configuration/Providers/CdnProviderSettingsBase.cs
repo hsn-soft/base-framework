061b54279af6d2bdf4c9e491f15162b46a4a716b
Hasan SAHIN <hasansahin@araskargo.com.tr>
2026-06-22 21:29:04 +0300

    ok

--- vpp/Hhs.Shared/Configuration/Providers/CdnProviderSettingsBase.cs
+++ vpp/Hhs.Shared/Configuration/Providers/CdnProviderSettingsBase.cs
@@ -2,7 +2,4 @@ namespace Hhs.Shared.Configuration.Providers;
 
 public class CdnProviderSettingsBase : ProviderSettingsBase
 {
-    public string ZonePath { get; set; } = "media";
-
-    public string PathPrefix { get; set; } = "prod";
 }
